eb791bf885f2a0594a50900bc0140db8fa56050a
Seif eldin mahmoud helmy <101514335+seif-hoppas@users.noreply.github.com>
2023-03-28 14:26:07 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+let header = `<div id="header">
+<div>
+  <ul>
+    <li style="float: left;padding: 10px;" id="facultyName"><label>Faculty of computers and artificial intelligence</label></li>
+    <ul>
+      <li><a href="about.html">About</a></li>
+      <li><a href="login.html">Login</a></li>
+      <li><a href="register.html">Register</a></li>
+      <li><a href="menu.html" class=""> Menu</a></li>
+      <li><a href="home.html">Home</a></li>
+
+    </ul>
+</ul></div>`
+
+document.getElementById('header').innerHTML=header;
+let footer = `<dev class="footer">
+<a href="Home.html">HOME</a>
+<a href="ABOUT.html">ABOUT</a>
+<a href="login.html">LOGIN</a>
+<a href="register.html"> REGISTER</a>
+<a href="menu.html"> MENU</a>
+</dev>`
+
+document.getElementById('footer').innerHTML=footer;
