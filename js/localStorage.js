506e22e4fbc7eb56f9dc2b3f8bb36d37a45da995
abdallhfattah <abdallh.fattah@33gmail.com>
2023-04-15 01:21:58 +0200

    fixed bugs

--- js/localStorage.js
+++ js/localStorage.js
@@ -1,6 +1,4 @@
-const studentArray = [];
-
-const submit_button = document.getElementById("btn");
+// const submit_button = document.getElementById("btn");
 
 function addStudent(){
     var ID = document.getElementById("id").value;
@@ -11,6 +9,7 @@ function addStudent(){
     var gender = document.getElementById("gender").value;
     var level = document.getElementById("lvl").value;
     var stat= document.getElementById("sta").value;
+    var email = document.getElementById("ema").value; // error
     var dep =  document.getElementById("department").value;
     var phone = document.getElementById("phone").value;
 
@@ -22,61 +21,38 @@ function addStudent(){
         student_gender : gender,
         student_lvl : level,
         student_stat : stat,
+        student_email : email,
         student_dep : dep,
         student_num : phone
     }
     console.log(student);
-    // // checking values if null
-    // if(ID !== "" && 
-    //     Name !== "" &&
-    //     student.student_dob !== "" &&
-    //     student.student_gpa !== "" &&
-    //     student.student_gender !== "select" &&
-    //     student.student_lvl !== "" &&
-    //     student.student_stat !== "select" &&
-    //     student.student_dep !== "select" &&
-    //     student.student_num !== "")
-    // {
-        let is_valid = true;
-        for(let i = 0; i < localStorage.length; i++){
-            const key = localStorage.key(i);
-            const stored_student = JSON.parse(localStorage.getItem(key));
-            if(student.student_id === stored_student.student_id){
-                alert("This ID already exists");
-                is_valid = false;
-                break;
-            }
-            else if(student.student_id === stored_student.student_email){
-                alert("This email already exists");
-                is_valid = false;
-                break;
-            }
-            else if(student.student_id === stored_student.student_num){
-                alert("This phone number already exists");
-                is_valid = false;
-                break;
-            }
+    // checking values if null
+    let is_valid = true;
+    for(let i = 0; i < localStorage.length; i++){
+        const key = localStorage.key(i);
+        const stored_student = JSON.parse(localStorage.getItem(key));
+        if(student.student_id === stored_student.student_id){
+            alert("This ID already exists");
+            is_valid = false;
+            break;
         }
-        if(is_valid){
-            localStorage.setItem(ID, JSON.stringify(student));
+        else if(student.student_email === stored_student.student_email){
+            alert("This email already exists");
+            is_valid = false;
+            break;
         }
-        else{
-            alert('aready exits idiit')
+        else if(student.student_num === stored_student.student_num){
+            alert("This phone number already exists");
+            is_valid = false;
+            break;
         }
     }
-    // else{
-    //     alert("Please fill the form");
-    // }
+    if(is_valid){
+        localStorage.setItem(ID, JSON.stringify(student));
+    }
+}
 
-
-
-submit_button.addEventListener("click", function() {
-    addStudent();
-});
-
-
-
-// let is_valid = true;
+// let is_valid = true
 // for(let i = 0; i < localStorage.length; i++){
 //     const key = localStorage.key(i);
 //     const stored_student = JSON.parse(localStorage.getItem(key));
