661a8ff6f0c6af38f9d1adb55162ea92f2246f16
abdallhfattah <abdallh.fattah@33gmail.com>
2023-04-15 22:05:30 +0200

    1.0V

--- js/localStorage.js
+++ js/localStorage.js
@@ -12,7 +12,6 @@ function addStudent(){
     var email = document.getElementById("ema").value; // error
     var dep =  document.getElementById("department").value;
     var phone = document.getElementById("phone").value;
-
     var student = {
         student_id : ID,
         student_name : Name,
@@ -25,9 +24,21 @@ function addStudent(){
         student_dep : dep,
         student_num : phone
     }
-    console.log(student);
+
     // checking values if null
+    if(student.student_id !== "" && 
+    student.student_name !== "" &&
+    student.student_dob !== "" &&
+    student.student_gpa !== "" &&
+    student.student_gender !== "" &&
+    student.student_lvl !== "" &&
+    student.student_stat !== "" &&
+    student.student_dep !== "" &&
+    student.student_num !== "")
+{
+    
     let is_valid = true;
+    // checking if ID , email , phone number is aready in use 
     for(let i = 0; i < localStorage.length; i++){
         const key = localStorage.key(i);
         const stored_student = JSON.parse(localStorage.getItem(key));
@@ -39,7 +50,7 @@ function addStudent(){
         else if(student.student_email === stored_student.student_email){
             alert("This email already exists");
             is_valid = false;
-            break;
+            break;  
         }
         else if(student.student_num === stored_student.student_num){
             alert("This phone number already exists");
@@ -47,28 +58,9 @@ function addStudent(){
             break;
         }
     }
+    // adding student if valid 
     if(is_valid){
         localStorage.setItem(ID, JSON.stringify(student));
     }
+    }
 }
-
-// let is_valid = true
-// for(let i = 0; i < localStorage.length; i++){
-//     const key = localStorage.key(i);
-//     const stored_student = JSON.parse(localStorage.getItem(key));
-//     if(student.student_id === stored_student.student_id){
-//         alert("This ID already exists");
-//         is_valid = false;
-//         break;
-//     }
-//     else if(student.student_id === stored_student.student_email){
-//         alert("This email already exists");
-//         is_valid = false;
-//         break;
-//     }
-//     else if(student.student_id === stored_student.student_num){
-//         alert("This phone number already exists");
-//         is_valid = false;
-//         break;
-//     }
-// }
