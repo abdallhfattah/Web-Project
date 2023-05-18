020976bbc65317429d10393155c74cd0f370fee7
abdallhfattah <abdallh.fattah@33gmail.com>
2023-05-19 00:03:30 +0300

    added Useradmin

--- base/models.py
+++ base/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
 class Department(models.Model):
     name = models.CharField(max_length=50,primary_key=True)
@@ -29,4 +31,9 @@ class student(models.Model):
 
 
 
+class UserAdmin(models.Model):
+    
+    user = models.OneToOneField(User, on_delete= models.CASCADE)
 
+    def __str__(self):
+        return self.user.username
