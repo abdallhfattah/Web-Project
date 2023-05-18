183331bcdc33607b535ffedb496357af79ea2c9e
abdallhfattah <abdallh.fattah@33gmail.com>
2023-05-19 00:47:26 +0300

    added User Admin model

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import student,Department
+from .models import student,Department,UserAdmin
 
 # Register your models here.
 admin.site.register(student)
 admin.site.register(Department)
+admin.site.register(UserAdmin)
