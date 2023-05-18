aceba60ec041af4d4881d3b511982564a358b6d4
abdallhfattah <abdallh.fattah@33gmail.com>
2023-05-19 00:48:49 +0300

    imported views

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+# TODO : only this is going to be imported (home, logout, register, login )
+# from base.views import home, logout, register, login
+from base import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('base.urls'))
+    path('', include('base.urls')),
+    # trying it
+    path('login/' , views.user_login),
 ]
