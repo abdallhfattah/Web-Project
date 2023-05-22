0a84eebb8c47de2c79c765686156cb2dfbfb8dc4
abdallhfattah <abdallh.fattah@33gmail.com>
2023-05-22 21:41:53 +0300

    added check_exsisting , select urls

--- base/urls.py
+++ base/urls.py
@@ -8,8 +8,10 @@ urlpatterns = [
     path('show/', views.show, name='show'),
     path('about/', views.about, name="about"),
     path('edit/', views.edit, name="edit"),
-    path('search/', views.getallstudents, name="search"),
+    path('check_existing/', views.check_existing, name="check_existing"),
     path('select/', views.select, name="select"),
+    path('search/', views.getallstudents, name="search"),
     path('add/', views.add, name="add"),
     path('user_login/', views.user_login, name="login"),
 ]
+
